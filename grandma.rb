3cfa1965d1f8a8dd49111d277ee174d5402c18d0
Thor Summerall <thor.summerall@gmail.com>
2020-11-17 01:46:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else phrase.downcase
     return puts "SPEAK UP, SONNY!"
   end
